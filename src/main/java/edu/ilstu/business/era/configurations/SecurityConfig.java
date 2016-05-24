1cd0ceeceec9b15e5e401d44cb4407cba9424c66
Basheer Becerra <bbecerr@outlook.com>
2016-05-23 21:17:50 -0500

    See #13 and #29 - Configured request authentication and login/logout
    -Configured authentication for ALL requests except login.
    -Configured the login and logout
    -Created the login page
    -Added a test user to the in-memory user store

---
+++ src/main/java/edu/ilstu/business/era/configurations/SecurityConfig.java
@@ -0,0 +1,32 @@
+package edu.ilstu.business.era.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Specific configuration of the security
+ * 
+ * @author Basheer
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+	}
+
+	// TODO: Verify that correct mapping is secured
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
+				.rememberMe().tokenValiditySeconds(2419200).key("katieEraKey").and().requiresChannel()
+				.antMatchers("/events/{eventId}/register").requiresSecure().and().logout().logoutSuccessUrl("/");
+	}
+}
