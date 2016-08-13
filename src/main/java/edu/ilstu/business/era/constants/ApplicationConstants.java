85bebc8878f94b42e3172da95b50140d60b754b6
Basheer Becerra <bbecerr@outlook.com>
2016-08-13 12:59:48 -0500

    see #30 - added KatieSecurityRiskException class

--- src/main/java/edu/ilstu/business/era/constants/ApplicationConstants.java
+++ src/main/java/edu/ilstu/business/era/constants/ApplicationConstants.java
@@ -2,10 +2,7 @@ package edu.ilstu.business.era.constants;
 
 public class ApplicationConstants {
 
-	/**
-	 * Defines the folder on which views to use.
-	 */
-	public static final String PAGE_FOLDER = "prod/";
+	public static final String PAGE_FOLDER = "devBACK/";
 	
 	public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ";
 }
